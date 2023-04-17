add360d372bc2c96182d41b9b567b9a09ca68c22
Salman Khan <salmankhan@Salmans-MacBook-Pro.local>
2023-04-18 02:04:42 +0530

    add screens

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -4,6 +4,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Wellcome from '../screen/Wellcome';
 import LoginWithPhone from '../screen/LoginWithPhone';
+import Otp from '../screen/Otp';
+import Profile from '../screen/Profile';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +15,9 @@ const AuthStack = () => {
       <Stack.Navigator screenOptions={{headerShown: false}}>
         {/* <Stack.Screen name="Wellcome" component={Wellcome} /> */}
         <Stack.Screen name="LoginWithPhone" component={LoginWithPhone} />
+        <Stack.Screen name="Otp" component={Otp} />
+        <Stack.Screen name="Profile" component={Profile} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screen/Otp.js
@@ -0,0 +1,79 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import OTPInputView from '@twotalltotems/react-native-otp-input';
+
+import Container from '../comman/Container';
+import {COLORS, HP_WP, SIZE} from '../comman/theme';
+import GlobalHeader from '../comman/GlobalHeader';
+import GlobalButton from '../comman/GlobalButton';
+
+const Otp = ({navigation}) => {
+  const OTPRef = useRef(null);
+  const [clearOTP, setClearOTP] = useState(false);
+  const [otp, setOtp] = useState('');
+  const [otpError, setOtpError] = useState('');
+
+  return (
+    <Container Style={styles.container}>
+      <GlobalHeader />
+      <View style={styles.innarMainContainer}>
+        <Text style={styles.smsCode}>Enter SMS Code </Text>
+        <OTPInputView
+          style={styles.OtpInputStyle}
+          pinCount={4}
+          keyboardType={'phone-pad'}
+          // code={this.state.code} //You can supply this prop or not. The component will be used as a controlled / uncontrolled component respectively.
+          // onCodeChanged = {code => { this.setState({code})}}
+          autoFocusOnLoad
+          codeInputFieldStyle={styles.underlineStyleBase}
+          onCodeFilled={code => {
+            console.log(`Code is ${code}, you are good to go!`);
+          }}
+        />
+        <Text style={styles.bottomText}>RESEND</Text>
+        <GlobalButton
+          Style={styles.buttonStyle}
+          title={'CONTINUE'}
+          onPress={() => navigation.navigate('Profile')}
+        />
+      </View>
+    </Container>
+  );
+};
+
+export default Otp;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: HP_WP.wp(5),
+  },
+  innarMainContainer: {
+    marginHorizontal: HP_WP.wp(7),
+  },
+  smsCode: {
+    fontSize: SIZE.XT,
+    color: COLORS.blk,
+    marginTop: HP_WP.hp(3),
+  },
+  OtpInputStyle: {
+    height: HP_WP.hp(6),
+    marginTop: HP_WP.hp(4),
+  },
+  underlineStyleBase: {
+    width: HP_WP.wp(15),
+    borderWidth: 0,
+    borderBottomWidth: 1.5,
+    borderBottomColor: '#8E8E8E',
+    color: COLORS.black,
+    fontSize: SIZE.L,
+  },
+  bottomText: {
+    color: '#8C8C8C',
+    fontSize: SIZE.M,
+    marginTop: HP_WP.hp(3),
+    textAlign: 'center',
+  },
+  buttonStyle: {
+    marginTop: HP_WP.hp(3),
+  },
+});
