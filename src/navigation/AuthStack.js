c336f31a10c313824d25e4137adc077b07aa6e33
“hanifmohammad” <thenanifmohammad@gmail.com>
2023-04-18 02:08:01 +0530

    zustand add

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Wellcome from '../screen/Wellcome';
 import LoginWithPhone from '../screen/LoginWithPhone';
+import LoginWithFacebook from '../screen/LoginWithFacebook';
 
 const Stack = createNativeStackNavigator();
 
 const AuthStack = () => {
   return (
-    <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Wellcome" component={Wellcome} />
-        {/* <Stack.Screen name="LoginWithPhone" component={LoginWithPhone} /> */}
+        <Stack.Screen name="LoginWithFacebook" component={LoginWithFacebook} />
       </Stack.Navigator>
-    </NavigationContainer>
   );
 };
 
