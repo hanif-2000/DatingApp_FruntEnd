9750ff014644df7ff80952c2a0b42f7a58555e92
“hanifmohammad” <thenanifmohammad@gmail.com>
2023-04-18 10:25:21 +0530

    ios font add

--- src/screen/LoginWithFacebook.js
+++ src/screen/LoginWithFacebook.js
@@ -5,6 +5,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import {COLORS, Font, HP_WP, IMAGE, SIZE} from '../comman/theme';
 import Container from '../comman/Container';
 import {Text} from 'react-native-elements';
+import GlobalButton from '../comman/GlobalButton';
 
 const LoginWithFacebook = () => {
   return (
@@ -14,19 +15,17 @@ const LoginWithFacebook = () => {
         end={{x: 0.9, y: 0.9}}
         colors={['#AA3FEC', '#000000']}
         style={styles.linearGradient}>
-        <Image
-          source={IMAGE.Logo}
-          style={{
-            height: HP_WP.hp(15),
-            width: HP_WP.wp(30),
-            alignSelf: 'center',
-            marginTop: HP_WP.hp(20),
-          }}
-        />
-        <Text style={{fontSize:SIZE.N,color:COLORS.white,fontFamily:Font.semiBold,textAlign:'center'}}>
+        <Image source={IMAGE.Logo} style={styles.logo} />
+        <Text style={styles.pragraf}>
           By clicking Log In, you agree with our Terms. Learn how we process
           your data in our Privacy Policy and Cookies Policy.
         </Text>
+        <GlobalButton
+          icon
+          textStyle={{color: COLORS.black}}
+          title={'Login With facebook'}
+          Style={{backgroundColor: COLORS.white}}
+        />
       </LinearGradient>
     </Container>
   );
@@ -36,4 +35,17 @@ export default LoginWithFacebook;
 
 const styles = StyleSheet.create({
   linearGradient: {flex: 1, paddingHorizontal: HP_WP.wp(10)},
+  pragraf: {
+    fontSize: SIZE.N,
+    color: COLORS.white,
+    fontFamily: Font.semiBold,
+    textAlign: 'center',
+    marginVertical: HP_WP.hp(15),
+  },
+  logo: {
+    height: HP_WP.hp(15),
+    width: HP_WP.wp(30),
+    alignSelf: 'center',
+    marginTop: HP_WP.hp(20),
+  },
 });
