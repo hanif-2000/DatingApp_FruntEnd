7c5b7652b0a4ef50093a4b15824d2ee962dcbe09
“hanifmohammad” <thenanifmohammad@gmail.com>
2023-04-20 17:16:32 +0530

    android error

--- src/common/GlobalHeader.js
+++ src/common/GlobalHeader.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Icon } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {Icon} from 'react-native-elements';
+import {useNavigation} from '@react-navigation/native';
 
-import { COLORS, Font, HP_WP, IMAGE, SIZE } from './theme';
+import {COLORS, Font, HP_WP, IMAGE, SIZE} from './theme';
 
 const GlobalHeader = ({
   title,
@@ -17,16 +24,25 @@ const GlobalHeader = ({
   withoutIcon,
   disabled,
   logo,
-  mainContainer
+  mainContainer,
+  onPress,
+  drawerPress,
 }) => {
   let Route = useNavigation();
   return (
-    <View style={[styles.mainContainer, mainContainer, { paddingTop: Platform.OS === 'android' ? HP_WP.hp(2) : 0, }]}>
-      <View style={{ flex: 0.2, alignItems: 'flex-start' }}>
-        <TouchableOpacity disabled={disabled} onPress={() => Route.goBack()}>
+    <View
+      style={[
+        styles.mainContainer,
+        mainContainer,
+        {paddingTop: Platform.OS === 'android' ? HP_WP.hp(2) : HP_WP.hp(0.5)},
+      ]}>
+      <View style={{flex: 0.2, alignItems: 'flex-start'}}>
+        <TouchableOpacity
+          disabled={disabled}
+          onPress={drawerPress ? onPress : () => Route.goBack()}>
           <Icon
             name={drawer ? 'menu' : withoutIcon ? '' : 'arrow-back'}
-            type="ionicons"
+            type={'ionicons'}
             size={25}
             color={light ? '#fff' : COLORS.black}
           />
@@ -37,12 +53,15 @@ const GlobalHeader = ({
       ) : (
         <Text style={[styles.titles, headerTitles]}>{title}</Text>
       )}
-      <View style={{ flex: 0.2 }}>
+      <View style={{flex: 0.2}}>
         {rightImage && (
           <TouchableOpacity
             onPress={onPressRight}
-            style={{ alignSelf: 'flex-end' }}>
-            <Image source={rightIcon} style={[styles.rightIconStyle, rightIconStyle]} />
+            style={{alignSelf: 'flex-end'}}>
+            <Image
+              source={rightIcon}
+              style={[styles.rightIconStyle, rightIconStyle]}
+            />
           </TouchableOpacity>
         )}
       </View>
@@ -55,16 +74,14 @@ const styles = StyleSheet.create({
   mainContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    // paddingTop: HP_WP.hp(2),
     paddingBottom: HP_WP.hp(0.5),
-    paddingLeft: HP_WP.hp(0.8),
   },
   titles: {
     color: COLORS.black,
     fontSize: SIZE.XL,
     textAlign: 'center',
     flex: 1,
-    fontFamily: Font.semiBold
+    fontFamily: Font.semiBold,
   },
   logo: {
     height: HP_WP.hp(3),
@@ -76,5 +93,5 @@ const styles = StyleSheet.create({
   rightIconStyle: {
     height: HP_WP.hp(3),
     width: HP_WP.wp(6),
-  }
+  },
 });
